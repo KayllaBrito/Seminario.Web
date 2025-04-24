878260fbb1f6cf2782c7722b9a356e776cf56224
KayllaBrito <160272674+KayllaBrito@users.noreply.github.com>
2025-04-24 19:20:09 -0400

    Web

---
+++ Script.js
@@ -0,0 +1,12 @@
+document.getElementById('Botao').addEventListener('click', () => {
+    const gif= document.getElementById('gif');
+
+    const giff = document.createElement('img');
+    giff.src = 'https://www.gifcen.com/wp-content/uploads/2023/08/shrek-gif-5.gif';
+    giff.alt = 'GIF Dançante'
+
+    gif.appendChild(giff);
+})
+/*Kaylla Stéfany Teixeira de Brito
+    Tainara Rodrigues de Jesus
+    3°A Informática - 2025*/ 
