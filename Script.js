71d3bf368071709c246e02608021271bf2c898e0
KayllaBrito <160272674+KayllaBrito@users.noreply.github.com>
2025-04-25 10:59:29 -0400

    Edicao

--- Script.js
+++ Script.js
@@ -1,11 +1,12 @@
+/*Quando o botao com ID 'botao' dor clicado, executa a funçao*/
 document.getElementById('Botao').addEventListener('click', () => {
-    const gif= document.getElementById('gif');
+    const gif= document.getElementById('gif'); /*pega o (div) com ID do 'gif', onde o GIF será inserido */
 
-    const giff = document.createElement('img');
-    giff.src = 'https://www.gifcen.com/wp-content/uploads/2023/08/shrek-gif-5.gif';
-    giff.alt = 'GIF Dançante'
+    const giff = document.createElement('img'); /*Cria um novo elemento de imagem*/ 
+    giff.src = 'https://www.gifcen.com/wp-content/uploads/2023/08/shrek-gif-5.gif'; /*Define o link do GIF que irá aparecer na tela*/ 
+    giff.alt = 'GIF Dançante' /*O texto irá aparecer na tela, caso a imagem não carregar*/ 
 
-    gif.appendChild(giff);
+    gif.appendChild(giff); /*Adiciona o GIF no (div), fazendo ele aparecer na tela*/ 
 })
 /*Kaylla Stéfany Teixeira de Brito
     Tainara Rodrigues de Jesus
